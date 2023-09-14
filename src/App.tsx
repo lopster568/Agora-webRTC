fb7d71f6dc58d8bb1debde583f6a8c1749e06073
Roshan Singh <rosh.s568@gmail.com>
2023-09-15 00:49:26 +0530

    RTM chat added | RTC video calling is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import AgoraUIKit from 'agora-react-uikit';
+import Chat from './chat.component';
+
 const AgoraUI = () => {
     const [videoCall, setVideoCall] = useState(true);
-const rtcProps = {
-        appId: '15a52d431faa47b6a2c686684c45d0b4',
+    const rtcProps = {
+        appId: 'bab4231274ee4b3ab2158d1969a6dcfd',
         channel: 'test',
         token: null
     };
-const callbacks = {
+    const callbacks = {
         EndCall: () => setVideoCall(false),
     };
-const rtmProps = {};
-const styleProps = {};
-return videoCall ? (
+    const rtmProps = {
+
+    };
+    const styleProps = {};
+    return videoCall ? (
         <div style={{ display: 'flex', width: '100vw', height: '100vh' }}>
-            <h1>WebRTC Agora</h1>
             <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} rtmProps={rtmProps} styleProps={styleProps} />
+            <div style={{ width: "30%"}} >
+                <Chat />
+            </div>
         </div>
     ) : (
         <h3 onClick={() => setVideoCall(true)}>Start Call</h3>
