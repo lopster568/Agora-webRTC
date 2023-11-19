6fc50a6d8529941250e8f0be8a47734c6d4f4d66
Roshan Singh <rosh.s568@gmail.com>
2023-11-19 23:43:14 +0530

    Last commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const App = () => {
             ) : (
                 <ChannelForm setInCall={setInCall} setChannelName={setChannelName} />
             )}
+            <Chat />
         </div>
     );
 };
